781dfc8be064fec5a585cbca880106726c4b8635
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-20 10:48:48 -0300

    feat: Implementação no APP

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import base.SingletonEager;
+import base.SingletonLazeHolder;
+import base.SingletonLazy;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        SingletonLazy lazy = SingletonLazy.getInstance();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstance();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstance();
+        System.out.println(eager);
+        eager = SingletonEager.getInstance();
+        System.out.println(eager);
+
+        SingletonLazeHolder lazyHolder = SingletonLazeHolder.getInstance();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazeHolder.getInstance();
+        System.out.println(lazyHolder);
     }
 }
