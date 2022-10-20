c1e39acddf5f26c29d1038ec9c708508d2f95737
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-20 10:48:36 -0300

    feat: SingletonLazeHolder

---
+++ src/base/SingletonLazeHolder.java
@@ -0,0 +1,15 @@
+package base;
+
+public class SingletonLazeHolder {
+  private static class InstanceHolder {
+    public static SingletonLazeHolder instancia = new SingletonLazeHolder();
+  }
+
+  private SingletonLazeHolder() {
+    super();
+  }
+
+  public static SingletonLazeHolder getInstance() {
+    return InstanceHolder.instancia;
+  }
+}
