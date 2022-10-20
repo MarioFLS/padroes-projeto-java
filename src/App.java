5650cac61d02d335c954b63e0dffd9106c21571e
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-20 11:33:29 -0300

    feat: Implementado na tela principal

--- src/App.java
+++ src/App.java
@@ -1,9 +1,17 @@
 import base.singleton.SingletonEager;
 import base.singleton.SingletonLazeHolder;
 import base.singleton.SingletonLazy;
+import base.strategy.Compartamento;
+import base.strategy.ComportamentoAgressivo;
+import base.strategy.ComportamentoDefensivo;
+import base.strategy.ComportamentoNormal;
+import base.strategy.Robo;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+        // Singleton
+
         SingletonLazy lazy = SingletonLazy.getInstance();
         System.out.println(lazy);
         lazy = SingletonLazy.getInstance();
@@ -18,5 +26,24 @@ public class App {
         System.out.println(lazyHolder);
         lazyHolder = SingletonLazeHolder.getInstance();
         System.out.println(lazyHolder);
+
+        // Strategy
+        Compartamento normal = new ComportamentoNormal();
+        Compartamento devensivo = new ComportamentoDefensivo();
+        Compartamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+
+        robo.setComportamento(devensivo);
+        robo.mover();
+        robo.mover();
+
     }
 }
