7ba0582d778a2b53d0659dde4c55b06b6540d084
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-20 10:41:12 -0300

    feat: SingletonEager

---
+++ src/base/SingletonEager.java
@@ -0,0 +1,19 @@
+package base;
+
+/**
+ * Singleton "apressado"
+ * 
+ * @author Mario
+ */
+
+public class SingletonEager {
+  private static SingletonEager instancia = new SingletonEager();
+
+  private SingletonEager() {
+    super();
+  }
+
+  public static SingletonEager getInstance() {
+    return instancia;
+  }
+}
