37c71d1eeeb2045754bb3884229d3f118ed3053e
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-20 10:40:46 -0300

    feat: SingletonLazy

---
+++ src/base/SingletonLazy.java
@@ -0,0 +1,23 @@
+package base;
+
+/**
+ * Singleton "preguiçoso"
+ * 
+ * @author Mario
+ */
+
+public class SingletonLazy {
+  private static SingletonLazy instancia;
+
+  private SingletonLazy() {
+    super();
+  }
+
+  public static SingletonLazy getInstance() {
+    if (instancia == null) {
+      instancia = new SingletonLazy();
+    }
+    return instancia;
+  }
+
+}
